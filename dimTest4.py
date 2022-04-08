c9cede734a58129790ddb9d64120ed32fc15be04
Ramiro Melero <ramiromelero@gmail.com>
2022-04-08 16:39:53 -0300

    Create dimTest4.py

---
+++ dimTest4.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Scalar operations
+
+scalarOperation = np.array([[1,2,3],[-1,-2,-3]])
+print("Scalar Operation '+' : ", scalarOperation + 2)
+
+scalarOperation = np.array([[1,2,3],[-1,-2,-3]])
+print("Scalar Operation '-' : ", scalarOperation - 2)
+
+scalarOperation = np.array([[1,2,3],[-1,-2,-3]])
+print("Scalar Operation '*' : ", scalarOperation * 2)
+
+scalarOperation = np.array([[1,2,3],[-1,-2,-3]])
+print("Scalar Operation '/' : ", scalarOperation / 2) # Float
+print("Scalar Operation '//' : ", scalarOperation // 2) # Int
+
+scalarOperation = np.array([[1,2,3],[-1,-2,-3]])
+print("Scalar Operation '**2' : ", scalarOperation **2)
