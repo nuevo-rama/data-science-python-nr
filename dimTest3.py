ea021bd426eed3b97b20923fb680fafe732fa88e
Ramiro Melero <ramiromelero@gmail.com>
2022-04-08 16:41:30 -0300

    Create dimTest3.py

---
+++ dimTest3.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+zArray = np.zeros(10) # Float
+print(zArray) 
+
+zArray = zArray.astype(int) # Int
+print(zArray)
+
+onesArray = np.ones(10) # Float
+print(onesArray) 
+
+onesArray = onesArray.astype(int) # Int
+print(onesArray)
+
+numArray = np.full(10,5)
+print(numArray)
