6d08b3cf3eb30f132bc8571e1da2e944efbd66a0
Ramiro Melero <ramiromelero@gmail.com>
2022-04-09 01:08:02 -0300

    Create dimTest7.py

---
+++ dimTest7.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# min() Returns the minimun value of the array
+matrixOne = np.array([[1,2,3],[4,5,6],[0,0,0]])
+print(np.min(matrixOne))
+
+# max() Returns the maximum value of the array
+matrixOne = np.array([[1,2,3],[4,5,6],[0,0,0]])
+print(np.max(matrixOne))
+
+# sum() Returns the sum of all values
+matrixOne = np.array([[1,2,3],[4,5,6],[0,0,0]])
+print(np.sum(matrixOne))
+
+# mean() Returns the mean of all values
+matrixOne = np.array([[1,2,3],[4,5,6],[0,0,0]])
+print(np.mean(matrixOne))
+
+# std() Returns the standard deviation
+matrixOne = np.array([[1,2,3],[4,5,6],[0,0,0]])
+print(np.std(matrixOne))
+
+# median() Returns the median
+matrixOne = np.array([[1,2,3],[4,5,6],[0,0,0]])
+print(np.median(matrixOne))
+
+
+
+
+
+
+
+
+
+
+
+
+
