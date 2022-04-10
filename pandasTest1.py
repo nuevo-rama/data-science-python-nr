64416373937fafd98f8bcc4f14ae1e4c069289b1
Ramiro Melero <ramiromelero@gmail.com>
2022-04-10 01:46:15 -0300

    Update pandasTest1.py

--- pandasTest1.py
+++ pandasTest1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+# Data Frames from Lists
+
 newList = [['a','b','c'],['d','e','f'],['g','h','i']]
 newDataFrame = pd.DataFrame(newList)
 print(newDataFrame)
@@ -9,6 +11,9 @@ newList = [['1982','01','01'],['1983','01','01'],['1984','01','01']]
 newDataFrame = pd.DataFrame(newList, columns=["Year", "Month", "Day"])
 print(newDataFrame)
 
-newListNp = np.array([[1,2,3],[4,5,6],[0,0,0]])
-newDataFrameNp = pd.DataFrame(newListNp)
+
+# Data Frames from numPy arrays
+
+newArrayNp = np.array([[1,2,3],[4,5,6],[0,0,0]])
+newDataFrameNp = pd.DataFrame(newArrayNp)
 print(newDataFrameNp)
