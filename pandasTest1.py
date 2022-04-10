e7679938a5d05edbf0ca9e687ac134be76c2513f
Ramiro Melero <ramiromelero@gmail.com>
2022-04-10 01:42:46 -0300

    Update pandasTest1.py

--- pandasTest1.py
+++ pandasTest1.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
 
+newList = [['a','b','c'],['d','e','f'],['g','h','i']]
+newDataFrame = pd.DataFrame(newList)
+print(newDataFrame)
+
+newList = [['1982','01','01'],['1983','01','01'],['1984','01','01']]
+newDataFrame = pd.DataFrame(newList, columns=["Year", "Month", "Day"])
+print(newDataFrame)
+
+newListNp = np.array([[1,2,3],[4,5,6],[0,0,0]])
+newDataFrameNp = pd.DataFrame(newListNp)
+print(newDataFrameNp)
