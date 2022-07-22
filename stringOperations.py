b337abf19c20b948945829206c30503981b3287c
Ramiro Melero <ramiromelero@gmail.com>
2022-07-22 14:55:44 -0300

    Update stringOperations.py

--- stringOperations.py
+++ stringOperations.py
@@ -5,3 +5,13 @@ print(string[0]) # result: s
 print(string[1]) # result: u
 
 print(string[2]) # result: n
+
+###
+
+print(string[-1]) # result : n
+
+###
+
+print(string[0:1]) # result: su
+
+print(string[1:2]) # result: un
